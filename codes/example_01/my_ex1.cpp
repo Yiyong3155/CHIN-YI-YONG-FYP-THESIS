91ac09d26a435b668b3a69d7ed4210f62cc68166
Adeeb Arif Kor <adeebkor@gmail.com>
2025-11-26 17:56:05 +0800

    Try more features

--- codes/example_01/my_ex1.cpp
+++ codes/example_01/my_ex1.cpp
@@ -1,7 +1,39 @@
-#include <nanobind/nanobind.h>
+#include <iostream>
+#include <string>
 
-int add(int a, int b) { return a + b; }
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
+
+namespace nb = nanobind;
+using namespace nb::literals;
+
+int add(int a, int b = 1) { return a + b; }
+int mult(int a, int b) { return a * b; }
+
+class FinalYearProject {
+public:
+    FinalYearProject(
+        std::string student_name
+    )
+    : name(student_name) {
+    }
+
+    void print_name() const {
+        std::cout << "The student name is " << name << "\n";
+    }
+
+private:
+    std::string name;
+};
 
 NB_MODULE(my_ex1, m) {
-    m.def("add", &add);
+    m.def("add", &add, "a"_a, "b"_a = 1,
+          "This function adds two numbers and increments if only one is provided.");
+    m.def("mult", &mult);
+    
+    nb::class_<FinalYearProject>(m, "FinalYearProject")
+        .def(nb::init<std::string &>())
+        .def("print_name", &FinalYearProject::print_name);
+
+    m.doc() = "A simple example of nanobind";
 }
