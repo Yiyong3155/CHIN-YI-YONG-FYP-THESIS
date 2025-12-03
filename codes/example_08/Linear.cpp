8b0bb145221bebd52746846a250a6869c6984669
Adeeb Arif Kor <adeebkor@gmail.com>
2025-12-03 22:22:54 +0800

    Change to float

--- codes/example_08/Linear.cpp
+++ codes/example_08/Linear.cpp
@@ -8,6 +8,6 @@ template <typename T>
 LinearSpectral2D<T>::LinearSpectral2D(basix::FiniteElement<T> element) {};
 
 NB_MODULE(Linear, m) {
-    nb::class_<LinearSpectral2D<double>>(m, "LinearSpectral2D")
-        .def(nb::init<basix::FiniteElement<double>>());
+    nb::class_<LinearSpectral2D<float>>(m, "LinearSpectral2D")
+        .def(nb::init<basix::FiniteElement<float>>());
 }
