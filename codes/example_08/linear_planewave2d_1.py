b47c33f2e10b2b6c08233510bf263f3b385552d1
Adeeb Arif Kor <adeebkor@gmail.com>
2025-12-04 23:24:52 +0800

    Add example simulation

---
+++ codes/example_08/linear_planewave2d_1.py
@@ -0,0 +1,104 @@
+import numpy as np
+from mpi4py import MPI
+
+import basix
+import basix.ufl
+from dolfinx.fem import Function, functionspace
+from dolfinx.io import XDMFFile, VTXWriter
+from dolfinx import cpp
+
+from Linear import LinearSpectral2D
+
+# Source parameters
+sourceFrequency = 0.5e6  # (Hz)
+sourceAmplitude = 60000  # (Pa)
+period = 1 / sourceFrequency  # (s)
+
+# Material parameters
+speedOfSound = 1500  # (m/s)
+density = 1000  # (kg/m^3)
+
+# Domain parameters
+domainLength = 0.12  # (m)
+
+# FE parameters
+degreeOfBasis = 4
+
+# Read mesh and mesh tags
+with XDMFFile(MPI.COMM_WORLD, "../mesh.xdmf", "r") as fmesh:
+    mesh_name = "planewave_2d_1"
+    mesh = fmesh.read_mesh(name=f"{mesh_name}")
+    tdim = mesh.topology.dim
+    mt_cell = fmesh.read_meshtags(mesh, name=f"{mesh_name}_cells")
+    mesh.topology.create_connectivity(tdim - 1, tdim)
+    mt_facet = fmesh.read_meshtags(mesh, name=f"{mesh_name}_facets")
+
+# Mesh parameters
+numCell = mesh.topology.index_map(tdim).size_local
+hmin = np.array([cpp.mesh.h(mesh._cpp_object, tdim, np.arange(numCell)).min()])
+meshSize = np.zeros(1)
+MPI.COMM_WORLD.Reduce(hmin, meshSize, op=MPI.MIN, root=0)
+MPI.COMM_WORLD.Bcast(meshSize, root=0)
+
+# Define finite element
+cell_type = mesh.ufl_cell().cellname()
+element = basix.ufl.element(
+    basix.ElementFamily.P, cell_type, 4, basix.LagrangeVariant.gll_warped,
+    dtype=np.float64
+).basix_element
+
+# Define a DG function space for the physical parameters of the domain
+V_DG = functionspace(mesh, ("DG", 0))
+c0 = Function(V_DG)
+c0.x.array[:] = speedOfSound
+
+rho0 = Function(V_DG)
+rho0.x.array[:] = density
+
+# Temporal parameters
+CFL = 0.9
+timeStepSize = CFL * meshSize / (speedOfSound * degreeOfBasis**2)
+stepPerPeriod = int(period / timeStepSize + 1)
+timeStepSize = period / stepPerPeriod
+startTime = 0.0
+finalTime = (domainLength / speedOfSound + 4.0 / sourceFrequency)/6
+numberOfStep = int((finalTime - startTime) / timeStepSize + 1)
+
+if MPI.COMM_WORLD.rank == 0:
+    print("Problem type: Planewave 2D", flush=True)
+    print(f"Speed of sound: {speedOfSound}", flush=True)
+    print(f"Density: {density}", flush=True)
+    print(f"Source frequency: {sourceFrequency}", flush=True)
+    print(f"Source amplitude: {sourceAmplitude}", flush=True)
+    print(f"Domain length: {domainLength}", flush=True)
+    print(f"Polynomial basis degree: {degreeOfBasis}", flush=True)
+    print(f"Minimum mesh size: {meshSize[0]:4.4}", flush=True)
+    print(f"CFL number: {CFL}", flush=True)
+    print(f"Time step size: {timeStepSize:4.4}", flush=True)
+    print(f"Number of step per period: {stepPerPeriod}", flush=True)
+    print(f"Number of steps: {numberOfStep}", flush=True)
+
+# Instantiate model
+model = LinearSpectral2D(
+    element._e, 
+    mesh._cpp_object, 
+    mt_facet._cpp_object, 
+    c0._cpp_object,
+    rho0._cpp_object,
+    sourceFrequency,
+    sourceAmplitude,
+    speedOfSound)
+
+# Set initial condition
+model.init()
+
+# Run simulation
+model.rk4(startTime, finalTime, timeStepSize)
+
+# Field at final time
+uh = model.u_sol()
+
+print(type(uh))
+
+with VTXWriter(mesh.comm, "output_final.bp", uh) as f:
+    f.write(0.0)
